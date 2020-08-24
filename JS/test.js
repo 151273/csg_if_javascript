d40078d7b448ab6640443d309b961862ead17e27
151273 <151273@csgaugustinus.nl>
2020-08-24 11:00:21 +0000

    monday

--- JS/test.js
+++ JS/test.js
@@ -1,14 +1,16 @@
+function preload() {
+    plaatje = loadImage("../images/sprites/kever.png");
+}
+
 function setup() {
   canvas = createCanvas(450,450);
   background('silver');
+  textSize(30);
   canvas.parent('processing');
-  //noLoop();
+}
+ 
+function draw() {
+  image(plaatje,0,45,50,50);
+  text("breedte= "+plaatje.width,150,40);
 }
 
-function draw() {
-  noStroke();
-  fill('steelblue');
-  ellipse(0,0,800);
-  fill('deepskyblue');
-  ellipse(450,450,400);
-}
